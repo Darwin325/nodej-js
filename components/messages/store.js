51fbb2afca6c4c6f81cf2d4fb7b393b174c48c60
Darwin Zamora <darwinzc325@gmail.com>
2020-07-01 21:00:06 -0500

    Conectando con MongoDb, agregando y consultando mensajes.

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,10 +1,23 @@
-const list = []
+const db = require('mongoose')
+const Model = require('./model')
+
+//mongodb+srv://node-js:<password>@cluster0.2mzwc.mongodb.net/<dbname>?retryWrites=true&w=majority
+db.Promise = global.Promise
+db.connect('mongodb+srv://node-js:AUSeWNg5whspEnLO@cluster0.2mzwc.mongodb.net/Cluster0?retryWrites=true&w=majority',
+   { useNewUrlParser: true, useUnifiedTopology: true }
+   )
+
+console.log('[DB]: Conectada con éxito')
 
 const addMessage = message => {
-   list.push(message)
+   const myMessage = new Model(message)
+   myMessage.save()
 }
 
-const getMessage = () => list
+const getMessage = async () => {
+   const messages = await Model.find()
+   return messages
+}
 
 module.exports = {
    add: addMessage,
